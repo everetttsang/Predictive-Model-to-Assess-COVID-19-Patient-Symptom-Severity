1a7ce91725c9b2f38282dcfb11dca67a67dc8baf
everetttsang <everettt@usc.edu>
2020-11-27 01:24:01 -0800

    WORKING: search through entire publication database
    + sesarch through each column (abstract, body_text, back_matter, ref_entries)
    +wordcount
    +returns publication ID

--- hpc_wordcount.py
+++ hpc_wordcount.py
@@ -86,6 +86,30 @@ print(len(worklist))
 #print(count)
 count=0
 
+#split list into 4 separate workloads
+def splitlist(x):
+    #[[0-33587, 33588-67175, 67176-100764, 100765-134352]]
+    first = x[:33587]
+
+    second =x[33588:67175]
+    third = x[67176: 100764]
+    fourth = x[100765:]
+    liste = []
+    liste.append(first)
+
+    liste.append(second)
+
+    liste.append(third)
+
+    liste.append(fourth)
+
+    #print(len(liste))
+    return liste
+
+#worklist = split_lists(worklist, 4)
+#print(worklist)
+worklist = splitlist(worklist)
+
 dictionary_list = []
 #---------------------------------------------------
 #with open(worklist[0]) as l:
@@ -105,18 +129,141 @@ dictionary_list = []
 
 
 
-for item in worklist:
-    count=count+1
-    #print(count )
-    #print(item)
-    with open(item) as l:
-        temp = json.load(l)
-        dictionary_list.append(temp)
-    if len(dictionary_list)%1000==0:
-        print(len(dictionary_list))
-print("Done")
+for x in range(4):
+    for item in worklist[x]:
+        count=count+1
+        #print(count )
+        #print(item)
+        with open(item) as l:
+            temp = json.load(l)
+            dictionary_list.append(temp)
+        if len(dictionary_list)%1000==0:
+            print(len(dictionary_list))
+    if x==0:
+        df_final = pd.DataFrame.from_dict(dictionary_list)
+    else:
+        df_final = df_final.append(dictionary_list, ignore_index=True)
+    dictionary_list=[]
+    print("Done")
 #df_final = pd.DataFrame.from_dict(dictionary_list)
-#df_final
+df_final
     #data= data.append(temp, ignore_index=True)
 
 #data
+
+#add empty column "count"
+df_final["count"]=0
+df_final["freq_pn"]= float(0)
+df_final["freq_di"]= float(0)
+
+#print(df_final.dtypes)
+
+
+
+pattern = ["pneumonia","diabetes"]
+vals = 0
+
+
+
+
+#search through each article
+for x in df_final.index: #341712
+    #keywords count to search
+    pneumonia_count = 0;
+    diabetes_count = 0;
+    wordcount = 0
+
+    #search through abstract
+    for y in range(len(df_final.at[x, 'abstract'])):
+        #print(df_final.at[1, 'abstract'][y]['text'])
+
+        #preprossessing to clean up the text
+        abstractCurr = df_final.at[x, 'abstract'][y]['text']
+        abstractSplit = abstractCurr.split(" ")
+        #print(abstractSplit)
+        for word in abstractSplit:
+            wordcount += 1
+            if(pattern[0] in word):
+                pneumonia_count += 1
+            if(pattern[1] in word):
+                diabetes_count += 1
+
+
+    #search through body text
+    for y in range(len(df_final.at[x, 'body_text'])):
+        #print(df_final.at[1, 'abstract'][y]['text'])
+
+        #preprossessing to clean up the text
+        abstractCurr = df_final.at[x, 'body_text'][y]['text']
+        abstractSplit = abstractCurr.split(" ")
+        #print(abstractSplit)
+        for word in abstractSplit:
+            wordcount += 1
+            if(pattern[0] in word):
+                pneumonia_count += 1
+            if(pattern[1] in word):
+                diabetes_count += 1
+
+    #search through "back_matter"
+    for y in range(len(df_final.at[x, 'back_matter'])):
+        #print(df_final.at[1, 'abstract'][y]['text'])
+
+        #preprossessing to clean up the text
+        abstractCurr = df_final.at[x, 'back_matter'][y]['text']
+        abstractSplit = abstractCurr.split(" ")
+        #print(abstractSplit)
+        for word in abstractSplit:
+            wordcount += 1
+            if(pattern[0] in word):
+                pneumonia_count += 1
+            if(pattern[1] in word):
+                diabetes_count += 1
+
+    #search through captions to figures
+    for y in df_final.at[4, 'ref_entries']:
+        #preprossessing to clean up the text
+        abstractCurr = df_final.at[4, 'ref_entries'][y]['text']
+        abstractSplit = abstractCurr.split(" ")
+        #print(abstractSplit)
+        for word in abstractSplit:
+            wordcount += 1
+            if(pattern[0] in word):
+                pneumonia_count += 1
+            if(pattern[1] in word):
+                diabetes_count += 1
+
+    pneumonia_freq = float(pneumonia_count) / float(wordcount-pneumonia_count);
+    diabetes_freq = float(diabetes_count) / float(wordcount);
+
+#     df.at[x, 'count'] = count
+    df_final.at[x, 'freq_pn'] = pneumonia_freq
+    df_final.at[x, 'freq_di'] = diabetes_freq
+    df_final.at[x, 'count'] = wordcount
+
+df_final
+
+
+
+
+
+maxURL = []
+prevFreqs = []
+prevIndexes = []
+articleID = []
+currFreq = 0
+maxFreq = 0
+maxIndex = 0
+for i in range(0,10):
+    for x in df_final.index: #341712
+        if(df_final.at[x,'freq_pn'] > maxFreq and prevIndexes.count(x) == 0):
+            maxFreq = df_final.at[x,'freq_pn']
+            maxIndex = x
+    prevFreqs.append(maxFreq)
+    prevIndexes.append(maxIndex)
+    maxFreq = 0
+
+print(prevFreqs)
+print(prevIndexes)
+for j in prevIndexes:
+    articleID.append(df_final.at[j, 'paper_id'])
+print(articleID)
